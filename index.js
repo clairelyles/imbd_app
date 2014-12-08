0abdc1320532cc7e13402cd56b71a389956e3eca
clairelyles <claire.lyles@gmail.com>
2014-12-08 11:06:13 -0800

    updated remove button but still not working

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var bodyParser = require('body-parser');
 var db = require("./models/index.js");
 var request = require('request');
 var app = express();
+var methodOverride = require('method-override');
 
+app.use(methodOverride('X-HTTP-Method-Override'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + "/public"));
@@ -33,6 +35,44 @@ app.get('/search', function(req, res) {
 	})
 })
 
+// ------------- posts to watch list & creates new row in db ------------- //
+
+app.post('/watchlist', function(req, res) {
+	var myObject = req.body;
+	//console.log(myObject);
+
+	db.Queue.findOrCreate({where: {imbd_code: req.body.imdb_code, title: req.body.title, year: req.body.year }}).done(function(err,data,noCreated) {
+		db.Queue.findAll().done(function(err, data2) {
+		// Q for class: why is the array ordered not according to db id#?
+		//res.send({dataArray:data2})
+		res.render("watchlist", {dataArray: data2, msg: "Your movie has been added!"})
+		})
+	})
+})
+
+// ------------- posts to watch list & creates new row in db ------------- //
+
+app.get('/watchlist', function(req, res) {
+
+		db.Queue.findAll().done(function(err, data2) {
+		// Q for class: why is the array ordered not according to db id#?
+		//res.send({dataArray:data2})
+		res.render("watchlist", {dataArray: data2})
+		})
+	})
+
+// ------------- not working: removes from watch list ------------- //
+
+app.post('/watchlist', function(req, res) {
+	db.Queue.find({where: {imbd_code: req.body.imdb_code}}).then(function(Queue) {
+		Queue.destroy().success(function() {
+			res.redirect("/watchlist")
+		})
+	})
+})
+
+// last step: how do I link to the queue list? Unable to do the app.get since we're not sending anything over from the hidden input form fields?
+
 // ------------- renders show page ------------- //
 
 app.get('/:imdbID', function(req, res) {
@@ -50,32 +90,5 @@ app.get('/:imdbID', function(req, res) {
   		}
 	})
 })
-
-// ------------- posts to watch list & creates new row in db ------------- //
-
-app.post('/watchlist', function(req, res) {
-	var myObject = req.body;
-	//console.log(myObject);
-
-	db.Queue.findOrCreate({where: {imbd_code: req.body.imdb_code, title: req.body.title, year: req.body.year }}).done(function(err,data,noCreated) {
-		db.Queue.findAll().done(function(err, data2) {
-		// Q for class: why is the array ordered not according to db id#?
-		//res.send({dataArray:data2})
-		res.render("watchlist", {dataArray: data2})
-		})
-	})
-})
-
-// ------------- not working: removes from watch list ------------- //
-
-app.post('/watchlist', function(req, res) {
-	db.Queue.find({where: {imbd_code: req.body.imdb_code, title: req.body.title, year: req.body.year }}).then(function(row) {
-		row.destroy().success(function(err, data3) {
-			res.render("watchlist", {dataArray: data3})
-		})
-	})
-})
-
-// last step: how do I link to the queue list? Unable to do the app.get since we're not sending anything over from the hidden input form fields?
 
 app.listen(3000);
